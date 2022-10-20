e745c1b5ef0b8fd0f897ab6a779d84da65f26771
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-10-20 23:26:11 +0200

    update

---
+++ JS/pętle.js
@@ -0,0 +1,36 @@
+console.log("Hello World");
+
+console.log("JavaScript is awesome".toLocaleUpperCase());
+
+const age = 33;
+console.log(typeof age);
+
+
+// const falanga = prompt("Proszę wpisać poprawne hasło");
+// console.log(falanga);
+
+const x = 10;
+const y = 5;
+
+console.log(x - y);
+console.log(x + y);
+console.log(x * y);
+console.log(x / y);
+
+console.log(x == y);
+
+console.log(x != y);
+
+
+
+console.log(Number.parseFloat(25.8));
+
+console.log(10 + 5 + "305")
+console.log(1 + "2" + 4); // "124"
+console.log(1 + 2 + "4"); // "34"
+
+const message = "Dzień dobry, Tutaj Adrian Siwek";
+console.log(message.length);
+
+console.log(("sAmSuNg").toLocaleUpperCase() === "SAMSUNG");
+
