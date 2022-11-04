cb5936cd2059719e077509a9d7b2818159f07082
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-11-05 00:01:12 +0100

    Update function.js

---
+++ JS/function.js
@@ -0,0 +1,21 @@
+// function multiply(...args) {
+//   let total = 1;
+
+//   for (const argument of args) {
+//     // total = total * argument;
+//     total *= argument;
+//   }
+
+//   return total;
+// }
+
+// console.log(multiply(1, 2, 3, 4, 5)); //  120
+
+
+// const arr1 = [0, 1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8, 9];
+// const arr3 = [...arr1, ...arr2];
+
+// console.log(arr3);
+
+const a = 30;
