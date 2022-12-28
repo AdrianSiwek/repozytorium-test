40c6334e096b24abb040b4ea4301dc2c201d8008
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-28 14:50:31 +0100

    update task-10/JS and html

--- JS/task-10.js
+++ JS/task-10.js
@@ -1,5 +1,41 @@
-const gold = document.querySelector('.gold');
+const btnOne = document.querySelector('.btn1');
+const btnTwo = document.querySelector('.btn2');
+const square = document.querySelector('.square');
+const paraOne = document.querySelector('.p1');
+const paraTwo = document.querySelector('.p2');
 
-const childGold = document.createElement('div');
+const hello = () => {
+    console.log('Cześć')
+}
 
-gold.appendChild(childGold);
+const red = (event) => {
+  // highlight the mouseover target
+  event.target.style.background = "red";
+    
+}
+
+const blue = (event) => {
+    event.target.style.background = "royalblue";
+}
+const toggleClass = () => {
+    paraOne.classList.toggle('show');
+    paraTwo.classList.toggle('show');
+
+}
+// const addClass = () => {
+//     paraOne.classList.add('show')
+//     paraTwo.classList.remove('show')
+// }
+
+// const removeClass = () => {
+//     paraOne.classList.remove('show')
+//     paraTwo.classList.add('show')
+// }
+
+btnTwo.addEventListener('click', toggleClass);
+
+// btnTwo.addEventListener('click', removeClass);
+square.addEventListener('mouseleave', blue);
+square.addEventListener("mouseover", red);
+btnOne.addEventListener('dblclick', hello);
+
