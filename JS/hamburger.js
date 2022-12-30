99145e6ad0d9c0eab974571d1f74885166bc0584
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-30 13:40:07 +0100

    Update hamburger.js

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -3,3 +3,14 @@ const barsIcone = document.querySelector('.fa-bars');
 const xIco = document.querySelector('.fa-times');
 const nav = document.querySelector('nav ul');
 // const burgerBtn = document.querySelector('.burger');
+
+const handleNav = () => {
+    nav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+    xIco.classList.toggle('hide');
+    barsIcone.classList.toggle('hide');
+
+
+}
+
+burgerBtn.addEventListener('click', handleNav);
