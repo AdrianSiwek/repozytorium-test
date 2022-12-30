1e0e33deb16186edda5f8066ac4174674449765d
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2022-12-30 13:32:51 +0100

    Update hamburger.js

---
+++ JS/hamburger.js
@@ -0,0 +1,5 @@
+const burgerBtn = document.querySelector('.burger');
+const barsIcone = document.querySelector('.fa-bars');
+const xIco = document.querySelector('.fa-times');
+const nav = document.querySelector('nav ul');
+// const burgerBtn = document.querySelector('.burger');
