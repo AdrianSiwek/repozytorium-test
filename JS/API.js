931b30419be80aa83379bb12aeb493618b2f95c5
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-01-03 09:24:08 +0100

    update API. html and js

--- JS/API.js
+++ JS/API.js
@@ -1,11 +1,36 @@
-const clickBtn = document.querySelector('button');
-const img = document.querySelector('img');
+// const clickBtn = document.querySelector('button');
+// const img = document.querySelector('img');
+
+// const URL = "https://dog.ceo/api/breeds/image/random";
+
+// clickBtn.addEventListener('click', () => {
+//     fetch(URL)
+//         .then(response => response.json())
+//         .then(data => img.setAttribute('src', data.message))
+//         .catch(err => console.error(err))
+// })
+
+
+const ones = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
 
 const URL = "https://dog.ceo/api/breeds/image/random";
 
-clickBtn.addEventListener('click', () => {
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => img.setAttribute('src', data.message))
-        .catch(err => console.error(err))
-})
+fetch(URL)
+    .then(response => response.json())
+    .then(data => ones.setAttribute('src', data.message))
+    .catch(err => console.error(err))
+
+
+axios.get(URL).then(res => {
+        two.setAttribute('src', res.data.message);
+    })
+
+
+async function showImage() {
+    const response = await axios.get(URL)
+    three.setAttribute('src', response.data.message)
+}
+    
+showImage();
